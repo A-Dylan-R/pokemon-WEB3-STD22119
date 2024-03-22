0ed8bd55c68531517fc7af54206f45e0a596e36d
Aina Dylan Rasamoelina <hei.dylan.3@gmail.com>
2024-03-22 23:30:22 +0300

    feat: server.js / client.js

---
+++ app/client.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import PokemonList from '../components/PokemonList';
+
+const ClientPage = () => {
+    const [pokemons, setPokemons] = useState([]);
+
+    useEffect(() => {
+        const fetchPokemons = async () => {
+            const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=50&offset=0');
+            setPokemons(response.data.results);
+        };
+
+        fetchPokemons();
+    }, []);
+
+    return (
+        <div>
+            <PokemonList pokemons={pokemons} />
+        </div>
+    );
+};
+
+export default ClientPage;
